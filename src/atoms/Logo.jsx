3125f752f1e96b1d9430786f88e1eb452d3269c4
Rafli Ramadhan <rafliramdhn@gmail.com>
2024-07-04 12:11:41 +0700

    add textareaautosize

--- src/atoms/Logo.jsx
+++ src/atoms/Logo.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-function Logo() {
+function Logo(props) {
+  const {text, size} = props
   return (
-    <h1 className='font-bold text-xl my-2 py-2 px-3 relative'>
-      AI-INTERLINKED
+    <h1 className={`text-white font-bold ${size ? size : 'text-xl'} my-2 py-2 px-3 relative`}>
+      AI-INTERLINKED {text}
       <div className='w-4 h-4 bg-blue-700 rounded-full absolute left-0 top-0'></div>
     </h1>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import LayoutPage from '../templates/LayoutPage'
+import Logo from '../atoms/Logo'
+import Typewriter from 'typewriter-effect';
+import IconUp from '../atoms/icons/IconUp';
+import TextareaAutosize from 'react-textarea-autosize';
 
 function Home() {
   return (
     <LayoutPage>
-      <h1 className='text-2xl font-bold'>Home</h1>
+      <div class="flex h-full px-10">
+        <div class="my-auto">
+          <div className='flex flex-col -space-y-3 text-2xl pb-20'>
+            <Logo text="/ BAPPENAS GPT" size="text-2xl"/>
+            <span className='text-gray-400 font-medium pl-2 flex space-x-2'>
+              <span>Mulai tanyakan</span> <Typewriter
+              options={{
+                strings: ['prioritas pembangunan.', 'indikator pembangunan.', 'data pembangunan.', 'sasaran pembangunan.', 'target pembangunan.', 'program pembangunan.', 'kegiatan pembangunan.', 'output pembangunan.', 'dampak pembangunan.', 'evaluasi pembangunan.'],
+                autoStart: true,
+                loop: true,
+                delay: 30,
+                deleteSpeed: 10
+              }}
+            /> </span>
+            
+          </div>
+        </div>
+
+        <div className='fixed bottom-10 w-4/6 bg-accent px-2 py-2 rounded-lg'>
+          <div className='flex space-x-2 items-end'>
+            {/* <textarea className='w-full bg-white text-black input py-1 rounded-md resize-none' rows={10}></textarea> */}
+            <TextareaAutosize maxRows={3} minRows={1} className='w-full bg-transparent outline-none border-none shadow-none text-white input py-2 rounded-md resize-none'  />
+            <button className='bg-primary p-2 rounded-full'>
+              <IconUp/>
+            </button>
+          </div>
+        </div>
+      </div>
     </LayoutPage>
   )
 }
